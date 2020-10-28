6b3a2d30e1da95ea0a65756397bd7f5b011ab399
Rodrigo Gimenez <rodrigog83@gmail.com>
2020-10-28 12:27:45 +0100

    Remove Python2 unicode support

--- italian_utils/models.py
+++ italian_utils/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
 
-@python_2_unicode_compatible
 class Comune(models.Model):
     nome = models.CharField(max_length=255)
     codice_catastale = models.CharField(max_length=4, unique=True)
